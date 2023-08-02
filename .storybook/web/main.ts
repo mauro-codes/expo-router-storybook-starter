606995c6dc4893af329872df95bb3cede1784832
Mauro Garcia <maurogarcia0209@gmail.com>
2023-08-02 09:05:03 -0300

    Update main.ts on web to point to the right stories directory

--- .storybook/web/main.ts
+++ .storybook/web/main.ts
@@ -1,7 +1,7 @@
 module.exports = {
   stories: [
-    "../../src/components/**/*.stories.mdx",
-    "../../src/components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
